7dc28011605e8df0a6b74b8aae9c2a552e8ef2e7
Le Duong <duonglq@magenest.com>
2023-04-19 12:59:46 +0700

    update design

--- index.php
+++ index.php
@@ -58,27 +58,28 @@ include "header.php";
             </div>
             <div class="new_product-list wow fadeInUp" data-wow-delay="0.6s">
                 <div class="row scroll-snap gx-5">
+
                     <div class="col-lg-3 col-6 item">
                         <a class="new_product-item d-flex flex-column " href="#">
                             <div class="new_product-top">
                                 <div class="inner">
-                                <div class="new_product-img">
-                                    <img src="./assets/images/pro-img1.png" alt="">
+                                    <div class="new_product-img">
+                                        <img src="./assets/images/pro-img1.png" alt="">
+                                    </div>
+
+                                    <span class="discount">
+                                        GIẢM GIÁ 44%
+                                    </span>
+                                    <div class="promotion d-flex">
+                                        <span class="date">
+                                            15.03
+                                        </span>
+                                        <span class="text">
+                                            FREE DELIVERY
+                                        </span>
+                                    </div>
                                 </div>
 
-                                <span class="discount">
-                                    GIẢM GIÁ 44%
-                                </span>
-                                <div class="promotion d-flex">
-                                    <span class="date">
-                                        15.03
-                                    </span>
-                                    <span class="text">
-                                    FREE DELIVERY
-                                    </span>
-                                </div>
-                                </div>
-                               
                             </div>
                             <div class="new_product-bottom mt-auto">
                                 <div class="price">
@@ -102,23 +103,23 @@ include "header.php";
                         <a class="new_product-item d-flex flex-column " href="#">
                             <div class="new_product-top">
                                 <div class="inner">
-                                <div class="new_product-img">
-                                    <img src="./assets/images/pro-img2.png" alt="">
+                                    <div class="new_product-img">
+                                        <img src="./assets/images/pro-img2.png" alt="">
+                                    </div>
+
+                                    <span class="discount">
+                                        GIẢM GIÁ 44%
+                                    </span>
+                                    <div class="promotion d-flex">
+                                        <span class="date">
+                                            15.03
+                                        </span>
+                                        <span class="text">
+                                            FREE DELIVERY
+                                        </span>
+                                    </div>
                                 </div>
 
-                                <span class="discount">
-                                    GIẢM GIÁ 44%
-                                </span>
-                                <div class="promotion d-flex">
-                                    <span class="date">
-                                        15.03
-                                    </span>
-                                    <span class="text">
-                                    FREE DELIVERY
-                                    </span>
-                                </div>
-                                </div>
-                               
                             </div>
                             <div class="new_product-bottom mt-auto">
                                 <div class="price">
@@ -142,23 +143,23 @@ include "header.php";
                         <a class="new_product-item d-flex flex-column " href="#">
                             <div class="new_product-top">
                                 <div class="inner">
-                                <div class="new_product-img">
-                                    <img src="./assets/images/pro-img1.png" alt="">
+                                    <div class="new_product-img">
+                                        <img src="./assets/images/pro-img1.png" alt="">
+                                    </div>
+
+                                    <span class="discount">
+                                        GIẢM GIÁ 44%
+                                    </span>
+                                    <div class="promotion d-flex">
+                                        <span class="date">
+                                            15.03
+                                        </span>
+                                        <span class="text">
+                                            FREE DELIVERY
+                                        </span>
+                                    </div>
                                 </div>
 
-                                <span class="discount">
-                                    GIẢM GIÁ 44%
-                                </span>
-                                <div class="promotion d-flex">
-                                    <span class="date">
-                                        15.03
-                                    </span>
-                                    <span class="text">
-                                    FREE DELIVERY
-                                    </span>
-                                </div>
-                                </div>
-                               
                             </div>
                             <div class="new_product-bottom mt-auto">
                                 <div class="price">
@@ -182,23 +183,23 @@ include "header.php";
                         <a class="new_product-item d-flex flex-column " href="#">
                             <div class="new_product-top">
                                 <div class="inner">
-                                <div class="new_product-img">
-                                    <img src="./assets/images/pro-img2.png" alt="">
+                                    <div class="new_product-img">
+                                        <img src="./assets/images/pro-img2.png" alt="">
+                                    </div>
+
+                                    <span class="discount">
+                                        GIẢM GIÁ 44%
+                                    </span>
+                                    <div class="promotion d-flex">
+                                        <span class="date">
+                                            15.03
+                                        </span>
+                                        <span class="text">
+                                            FREE DELIVERY
+                                        </span>
+                                    </div>
                                 </div>
 
-                                <span class="discount">
-                                    GIẢM GIÁ 44%
-                                </span>
-                                <div class="promotion d-flex">
-                                    <span class="date">
-                                        15.03
-                                    </span>
-                                    <span class="text">
-                                    FREE DELIVERY
-                                    </span>
-                                </div>
-                                </div>
-                               
                             </div>
                             <div class="new_product-bottom mt-auto">
                                 <div class="price">
@@ -217,6 +218,113 @@ include "header.php";
                                 </div>
                             </div>
                         </a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="list__category">
+        <div class="row g-0">
+            <?php $t=0.2;?>
+            <?php 
+                for ($i = 0; $i < 4; $i++) {
+                    $t=$t+0.2;
+                    
+                ?>
+            <div class="col-md-6">
+                <div class="list__category-item wow fadeInUp" data-wow-delay="<?php echo $t ;?>s">
+                    <div class="category-img">
+                        <a href="">
+                            <img src="./assets/images/cat<?php echo $i+1;?>.png" alt="">
+                        </a>
+                    </div>
+                    <h2 class="category-name">
+                        Cạo râu
+                    </h2>
+                    <div class="desc">
+                        Giữ phong cách hàng ngày của bạn sang trọng và bắt kịp xu hướng với hơn 20
+                        kiểu
+                        lựa chọn của chúng tôi để bạn lựa chọn.
+                    </div>
+                    <div class="btn-buy">
+                        <a href="#">
+                            Mua ngay
+                        </a>
+                    </div>
+                </div>
+
+
+            </div>
+
+            <?php 
+            } 
+            ?>
+
+        </div>
+
+
+    </section>
+    <section class="partner">
+        <div class="container">
+            <div class="partner__content d-lg-flex d-block">
+                <div class="partner__content-left">
+                    <div class="inner">
+                        <h3 class="title wow fadeInUp">
+                            Những cộng sự của chúng ta
+                        </h3>
+                        <div class="desc wow fadeInUp" data-wow-delay="0.2s">
+                            On the other hand, we denounce with righteous indignation and dislike
+                            men who are so beguiled and demoralized by the charms of pleasure of the
+                            moment
+                        </div>
+                        <div class="list-parter wow fadeInUp" data-wow-delay="0.4s">
+                            <div class="row row-cols-4 g-md-5 g-2">
+                                <?php 
+                                 for ($i = 0; $i < 12; $i++) {
+                                 ?>
+                                <div class="col">
+                                    <div class="parter-img">
+                                        <img src="./assets/images/pn<?php echo $i+1;?>.png" alt="">
+                                    </div>
+                                </div>
+                                <?php 
+                                 } 
+                                ?>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="partner__content-right">
+                    <div class="row g-5">
+                        <div class="col-md-6">
+                            <div class="partner__item wow fadeInUp" data-wow-delay="0.2s">
+                                <div class="partner__item-img">
+                                    <picture>
+                                      <source media="(max-width:767px)" srcset="./assets/images/partner1-mb.png">
+                                      <img src="./assets/images/partner1.png" alt="">
+                                    </picture>
+                                    
+                                </div>
+                                <div class="partner__item-title">
+                                    Our<br>Achievements
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="partner__item wow fadeInUp" data-wow-delay="0.4s">
+                                <div class="partner__item-img">
+                                <picture>
+                                      <source media="(max-width:767px)" srcset="./assets/images/partner2-mb.png">
+                                      <img src="./assets/images/partner2.png" alt="">
+                                    </picture>
+                                </div>
+                                <div class="partner__item-title">
+                                    Our<br>Collaborators
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
